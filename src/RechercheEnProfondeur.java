6b0e7c11c373f23d311d32e7dbc9c26b23b0414f
AMAL <amal.bourahma@etu.univ-amu.fr>
2024-02-12 16:07:31 +0100

    mise à jour 2 // ignorer classe ouvertPile et ouvert

---
+++ src/RechercheEnProfondeur.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class RechercheEnProfondeur {
+    List<Etat> ferme = new ArrayList<>();
+    Stack<Etat> ouvert = new Stack<>() ;
+
+    public void heuristic() {
+        Etat etat;
+        //Initialisation de ferme
+        ferme = null;
+        //Ajout de l'état initial à l'ensemble Ouvert
+        ouvert.add(new Etat("initial",new ArrayList<>(Arrays.asList(0,0,0,0)),0));
+        while( !ouvert.isEmpty() && !estBut(ouvert.peek()) ){
+            etat = ouvert.peek();
+            ouvert.remove(etat);
+            ferme.add(etat);
+            for(Etat voisin : etat.getVoisins()){
+                if(!ferme.contains(voisin)){
+                    if(!ouvert.contains(voisin)){
+                        ouvert.add(voisin);
+                    }
+                }
+            }
+        }
+    }
+
+
+    // Fonction pour calculer l'heuristique d'un état
+    public static int heuristic(String etat) {
+        // Implémentez votre fonction heuristique ici
+        return 0; // Pour l'exemple, retourne simplement 0
+        }
+    public Boolean estBut(Etat etat){ return true; }
+    // Fonction pour récupérer le meilleur état dans l'ensemble Ouvert
+    /*public static State meilleur(PriorityQueue<State> ouvert) {
+        return ouvert.peek();
+        }*/
+
+}
